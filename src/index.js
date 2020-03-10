0273fa5d26af72d6b8327a0769c2bfa06763e1dd
Wouter Vroege <wouter@woutervroege.nl>
2020-03-10 15:54:56 +0100

    fixed: incorrect paths
    Signed-off-by: Wouter Vroege <wouter@woutervroege.nl>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { Properties } from './properties/properties-mixin.js';
-export { ObservedProperties } from './properties/observed-properties-mixin.js';
-export { DOMProperties } from './properties/dom-properties-mixin.js';
-export { ReflectedProperties } from './properties/reflected-properties-mixin.js';
+export { Properties } from './properties-mixin.js';
+export { ObservedProperties } from './observed-properties-mixin.js';
+export { DOMProperties } from './dom-properties-mixin.js';
+export { ReflectedProperties } from './reflected-properties-mixin.js';
