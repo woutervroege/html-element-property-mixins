292cc2c4a743953bd709b2fa48d8e983de37e5eb
Wouter Vroege <wouter@woutervroege.nl>
2020-03-16 14:07:54 +0100

    fix: pointing to non-existent file
    Signed-off-by: Wouter Vroege <wouter@woutervroege.nl>

--- src/addons/index.js
+++ src/addons/index.js
@@ -1,4 +1,3 @@
 export { PropertyChangedHandler } from './property-changed-handler-mixin.js';
 export { PropertiesChangedHandler } from './properties-changed-handler-mixin.js';
 export { PropertiesChangedCallback } from './properties-changed-callback-mixin.js';
-export { PropertyChangedEvent } from './property-changed-event-mixin.js';
